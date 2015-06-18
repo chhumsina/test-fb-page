79c593f8a46541ab9a03bf4cd1b5668fe886912a
Sina <chhumsina@gmail.com>
2015-06-18 17:13:17 +0700

    new render

--- cordova_plugins.js
+++ cordova_plugins.js
@@ -4,12 +4,22 @@ module.exports = [
         "file": "plugins/cordova-plugin-whitelist/whitelist.js",
         "id": "cordova-plugin-whitelist.whitelist",
         "runs": true
+    },
+    {
+        "file": "plugins/com.google.cordova.admob/www/AdMob.js",
+        "id": "com.google.cordova.admob.AdMob",
+        "clobbers": [
+            "window.AdMob"
+        ]
     }
 ];
 module.exports.metadata = 
 // TOP OF METADATA
 {
-    "cordova-plugin-whitelist": "1.0.0"
+    "cordova-plugin-whitelist": "1.0.0",
+    "com.google.cordova.admob": "2.7.7",
+    "com.rjfun.cordova.extension": "1.1.4",
+    "com.google.playservices": "19.0.0"
 }
 // BOTTOM OF METADATA
 });
